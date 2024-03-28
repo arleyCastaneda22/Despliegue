c1639c767a9dcd31b7b2f87d76b7104ec240d51b
Jhon Arley Castañeda Vergara <arleycastaneda22@gmail.com>
2024-03-28 13:52:37 -0500

    subida

--- src/app/services/servicios.service.ts
+++ src/app/services/servicios.service.ts
@@ -11,8 +11,8 @@ export class ServiciosService {
   private refresh$ = new Subject<void>
   constructor(private http:HttpClient) { }
 
-  private url = 'http://localhost:5000/api/servicios/'
-  private url2 ='http://localhost:5000/api/servicios/estado/'
+  private url = 'https://beautyapi-9rry.onrender.com/api/servicios/'
+  private url2 ='https://beautyapi-9rry.onrender.com/api/servicios/estado/'
 
   get refresh(){
     return this.refresh$;
